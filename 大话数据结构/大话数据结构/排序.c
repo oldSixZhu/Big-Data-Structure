fca547fcbf1a39ed649aa5b6cf94e4162f970919
朱玉顺 <zhuYuShun666666@163.com>
2017-06-18 18:01:51 +0800

    update

--- 大话数据结构/大话数据结构/排序.c
+++ 大话数据结构/大话数据结构/排序.c
@@ -191,9 +191,245 @@ void daDingDui(SqList *L,int s,int m){
 //归并排序
 //归并--将两个或两个以上的有序表组合成一个新的有序表
 //2路归并排序:n个数据,两两归并,直到得到一个长度为n的有序序列
+//总的时间复杂度为O(nlogn),空间复杂度为O(n+logn)
+//比较占用内存,但是稳定,效率高
+
+//归并函数
+void realGuiBing(int SR[],int TR1[],int s,int t);
+//排序函数
+void Merge(int SR[],int TR[],int s,int m,int t);
+
+//调用归并函数
 void guiBing(SqList *L){
-    
+    realGuiBing(L->r,L->r,1,L->length);
 }
+
+//递归函数,将 SR[s...t] 归并排序为 TR1[s...t]
+void realGuiBing(int SR[],int TR1[],int s,int t){
+    int m;
+    int TR2[MAXSIZE +1];
+    //相等就不用排序了
+    if (s == t)
+    {
+        TR1[s] = SR[s];
+    }
+    else
+    {
+        //将SR[s...t]平分为SR[s...m]和SR[m+1...t]
+        m = (s+t)/2;
+        //递归将SR[s...m]归并为有序的TR2[s...m]
+        realGuiBing(SR,TR2,s,m);
+        //递归将SR[m+1...t]归并为有序的TR2[m+1...t]
+        realGuiBing(SR,TR2,m+1,t);
+        //将TR2[s...m]和TR2[m+1...t]归并到TR1[s...t]
+        Merge(TR2,TR1,s,m,t);
+    }
+}
+
+//关键函数,排序并合并
+//将有序的SR[i...m] 和 SR[m+1...n]归并为有序的TR[i...n]
+void Merge(int SR[],int TR[],int s,int m,int t){
+    int j,k,l;
+    //将SR中数据由小到大归并入TR
+    for (j = m+1, k = s; s <= m && j <= t; k++)
+    {
+        if (SR[s] < SR[j])
+        {
+            TR[k] = SR[s++];
+        }
+        else
+        {
+            TR[k] = SR[j++];
+        }
+    }
+    //将剩余的SR[s...m]复制到TR
+    if (s <= m)
+    {
+        for (l = 0; l <= m-s; l++)
+        {
+            TR[k+1] = SR[s+1];
+        }
+    }
+    //将剩余的SR[j...t]复制到TR
+    if (j <= t)
+    {
+        for (l = 0; l < t-j; l++)
+        {
+            TR[k+1] = SR[j+1];
+        }
+    }
+}
+
+//----------------------
+//优化归并排序,非递归实现
+//非递归增加的函数
+void MergeAdd(int SR[], int TR[],int s,int n);
+//归并排序
+void guiBing2(SqList *L){
+    //申请内存空间
+    int *TR = (int *)malloc(L->length *sizeof(int));
+    int k = 1;
+    while (k < L->length)
+    {
+        MergeAdd(L->r, TR, k, L->length);
+        //子序列长度加倍
+        k = 2*k;
+        MergeAdd(TR, L->r, k, L->length);
+        //加倍
+        k = 2*k;
+    }
+}
+
+void MergeAdd(int SR[], int TR[],int s,int n){
+    int i = 1;
+    int j;
+    while (i <= n - 2 * s + 1)
+    {
+        //两两归并
+        Merge(SR, TR, i, i + s - 1, i + 2 *s -1);
+        i = i +2 *s;
+    }
+    //归并最后两个序列
+    if (i < n-s+1)
+    {
+        Merge(SR, TR, i, i+s-1, n);
+    }
+    //剩下的单数直接并入
+    else
+    {
+        for (j = i; j <= n; j++)
+        {
+            TR[j] = SR[j];
+        }
+    }
+}
+
+//希尔排序为直接插入排序的升级,属于插入排序类
+//堆排序为简单选择排序的升级,属于选择排序类
+//快速排序为冒泡排序的升级,属于交换排序类
+
+//快速排序
+//通过一趟排序将待排数据分割成独立的两部分,其中一部分key均比另一部分key小,分别对这两部分记录继续排序,最终完成排序
+//时间复杂度平均为O(nlogn)
+
+//快速排序
+void realKuaiSu(SqList *L,int low,int high);
+//找到枢轴值
+int fenKai(SqList *L ,int low,int high);
+//调用快速排序
+void kuaiSu(SqList *L){
+    realKuaiSu(L,1,L->length);
+}
+//快速排序
+void realKuaiSu(SqList *L,int low,int high){
+    int pivot;
+    if (low < high)
+    {
+        //将L->r[low...high]一分为二,算出枢轴值pivot
+        pivot = fenKai(L,low,high);
+        //对低子表递归排序
+        realKuaiSu(L,low,pivot-1);
+        //对高子表递归排序
+        realKuaiSu(L,pivot+1,high);
+    }
+}
+
+//找到枢轴值,交换顺序表L中子表的数据
+//将选取的枢轴值不断变换,将比它小的换到它的左边,比它大的换到它的右边,它也在交换中不断更改自己的位置,直到完全满足这个要求为止
+int fenKai(SqList *L ,int low,int high){
+    int pivotkey;
+    //用子表的第一个数据做枢轴数据
+    pivotkey = L -> r[low];
+    //从表的两端交替向中间扫描
+    while (low < high)
+    {
+        //比枢轴数据小的数据交换到低端
+        while (low < high && L->r[high] >= pivotkey)
+        {
+            high--;
+            int temp=L->r[low];
+            L->r[low]=L->r[high];
+            L->r[high]=temp;
+        }
+        //将比枢轴大的数据交换到高端
+        while (low < high && L->r[low] <= pivotkey)
+        {
+            low++;
+            int temp=L->r[low];
+            L->r[low]=L->r[high];
+            L->r[high]=temp;
+        }
+    }
+    return low;
+}
+
+//快速排序优化
+//1.选取枢轴pivotkey的优化
+//三数取中法:取左,中,右三个数据先排序,将中间数作为枢轴
+//九数取中法
+int getPivotkey(SqList *L,int high,int low){
+    int pivotkey;
+    //计算数组中间元素的下标
+    int m = low + (high - low)/2;
+    //交换左右端数据,保证左端较小
+    if (L->r[low] > L->r[high])
+    {
+        int temp=L->r[low];
+        L->r[low]=L->r[high];
+        L->r[high]=temp;
+    }
+    //交换中间与右端数据,保证中间较小
+    if (L->r[m] > L->r[high])
+    {
+        int temp=L->r[m];
+        L->r[m]=L->r[high];
+        L->r[high]=temp;
+    }
+    //交换中间与左端数据,保证左端较小
+    if (L->r[m] > L->r[low])
+    {
+        int temp=L->r[m];
+        L->r[m]=L->r[low];
+        L->r[low]=temp;
+    }
+    //L.r[row]为三个关键字中间值
+    pivotkey = L->r[low];
+    return pivotkey;
+}
+
+//2.优化不必要的交换
+int youHuaKuaiSu(SqList *L,int low,int high){
+    //取到枢轴
+    int pivotkey = getPivotkey(L, high, low);
+    //将枢轴备份
+    L->r[0] = pivotkey;
+    //从表的两端交替向中间扫描
+    while (low < high)
+    {
+        //比枢轴数据小的数据交换到低端
+        while (low < high && L->r[high] >= pivotkey)
+        {
+            high--;
+            //采用替换不是交换
+            L->r[low] = L->r[high];
+        }
+        //将比枢轴大的数据交换到高端
+        while (low < high && L->r[low] <= pivotkey)
+        {
+            low++;
+            //采用替换不是交换
+            L->r[high] = L->r[low];
+        }
+    }
+    //将枢轴数值替换回L.r[low]
+    L->r[low] = L->r[0];
+    //返回枢轴所在位置
+    return low;
+}
+
+//因为递归的使用,非常大的数组用快速排序,直接插入排序是简单排序中性能最好的
+
+
 
 
 
